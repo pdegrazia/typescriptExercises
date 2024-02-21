4587c4d23ac7f42e964add99c9a9dabf1335572b
Paolo <p.degrazia@gmail.com>
2024-02-21 22:20:06 +0000

    first palyndrome test passing

--- src/palyndrome/palyndrome.test.ts
+++ src/palyndrome/palyndrome.test.ts
@@ -2,5 +2,18 @@ const isPalyndromeFunction = require('./palyndrome');
 
 
 test('Should return it is a palyndrome', () => {
+    const palyndromeWord = 'anna';
+    expect(isPalyndromeFunction(palyndromeWord)).toBe(true);
+});
+
+test('Should return it is not a palyndrome', () => {
+    expect(true);
+});
+
+test('Should return it is a palyndrome with mixed lower and upper case', () => {
+    expect(true);
+});
+
+test('Should throw when argument is not a string', () => {
     expect(true);
 });
