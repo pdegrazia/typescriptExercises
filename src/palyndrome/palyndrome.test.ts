98772cbe42ce8f7ce57abebcde65ccbd80215579
Paolo <p.degrazia@gmail.com>
2024-02-21 23:09:47 +0000

    additional tests, still failing, since there is no implementation

--- src/palyndrome/palyndrome.test.ts
+++ src/palyndrome/palyndrome.test.ts
@@ -2,18 +2,20 @@ const isPalyndromeFunction = require('./palyndrome');
 
 
 test('Should return it is a palyndrome', () => {
-    const palyndromeWord = 'anna';
+    const palyndromeWord: string = 'anna';
     expect(isPalyndromeFunction(palyndromeWord)).toBe(true);
 });
 
 test('Should return it is not a palyndrome', () => {
-    expect(true);
+    const notPalyndromeWord: string = 'iamnotapalyndrome';
+    expect(isPalyndromeFunction(notPalyndromeWord)).toBe(false);
 });
 
 test('Should return it is a palyndrome with mixed lower and upper case', () => {
-    expect(true);
+    const mixedPalyndrome: string = 'baRarAb';
+    expect(isPalyndromeFunction(mixedPalyndrome)).toBe(true); 
 });
 
 test('Should throw when argument is not a string', () => {
-    expect(true);
+    expect(isPalyndromeFunction(6)).toThrow(Error);
 });
